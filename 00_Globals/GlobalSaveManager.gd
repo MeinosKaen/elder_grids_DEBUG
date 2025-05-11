b645828359568aa03b4402c3a66f403c343ae584
Meinos Kaen~ <jpde@tutanota.com>
2025-05-11 09:48:32 +0200

    I understand the logic in a save system now. Backing it up just in case.

---
+++ 00_Globals/GlobalSaveManager.gd
@@ -0,0 +1,32 @@
+extends Node
+
+const SAVE_PATH = "user://"
+
+signal game_loaded
+signal game_saved
+
+
+var current_save : Dictionary ={
+	scene_path = "",
+	player = {
+		hp = 1,
+		max_hp = 1,
+		pos_x = 0,
+		pos_y = 0
+	},
+	items = [],
+	persistence = [],
+	quests = [],
+}
+
+func save_game() -> void:
+	var file := FileAccess.open(SAVE_PATH + "save.sav", FileAccess.WRITE)
+	var save_json = JSON.stringify(current_save)
+	file.store_line(save_json)
+	game_saved.emit()
+	print("save_game")
+	pass
+
+func load_game() -> void:
+	print("load_game")
+	pass
