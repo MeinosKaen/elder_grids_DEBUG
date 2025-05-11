d430e123787fa57248a212bf90dbdcbbb48dcb42
Meinos Kaen~ <jpde@tutanota.com>
2025-05-11 22:33:36 +0200

    Added Passive before I forgot.

--- 03_gameplay/02_battle/battle_skills/base_battle_skill.gd
+++ 03_gameplay/02_battle/battle_skills/base_battle_skill.gd
@@ -5,5 +5,5 @@ class_name BattleSkill extends Node
 @export var battleskill_power : int = 1
 @export var battleskill_icon : Texture2D = null
 
-@export_enum("Physical Damage", "Ranged Damage", "Magic Damage", "Recovery Only", "Debuff Only", "Buff Only") var battleskill_type : String = "Physical Damage"
+@export_enum("Physical Damage", "Ranged Damage", "Magic Damage", "Recovery Only", "Debuff Only", "Buff Only", "Passive") var battleskill_type : String = "Physical Damage"
 @export_enum("Fire", "Ice", "Lightning") var battleskill_element : String = "Fire"
