b6541b4fe729a3852c5cca1c785d15975c1a658d
Meinos Kaen~ <jpde@tutanota.com>
2025-05-11 11:23:52 +0200

    LIttle by little. This is all the variables for now. Now a load system.

--- 00_Globals/GlobalSaveManager.gd
+++ 00_Globals/GlobalSaveManager.gd
@@ -8,12 +8,19 @@ signal game_saved
 
 var current_save : Dictionary ={
 	scene_path = "",
+	player_x = 0,
+	player_y = 0,
 	player_info = {
 		first_name = "",
 		last_name = "",
 		background = "",
 		player_context = "",
 		current_money = 1,
+	},
+	game_time_variables = {
+		game_time_seconds = 0,
+		game_time_hours = 0,
+		game_time_minutes = 0,
 	},
 	player_lvling_info = {
 		lvl = 1,
@@ -46,14 +53,12 @@ var current_save : Dictionary ={
 		mysteries = 1.0,
 	},
 	player_unique_topics = [],
-	game_time_variables = {
-		game_time_seconds = 0,
-		game_time_hours = 0,
-		game_time_minutes = 0,
-	},
 }
 
 func save_game() -> void:
+	update_path_base_time()
+	update_lvling_attributes()
+	update_topics()
 	var file := FileAccess.open(SAVE_PATH + "save.sav", FileAccess.WRITE)
 	var save_json = JSON.stringify(current_save)
 	file.store_line(save_json)
@@ -64,3 +69,50 @@ func save_game() -> void:
 func load_game() -> void:
 	print("load_game")
 	pass
+
+func update_path_base_time() -> void:
+	var path : String = ""
+	for n in get_tree().root.get_children():
+		if n is Level:
+			path = n.scene_file_path
+	current_save.scene_path = path
+	var current_player = PlayerManager.player
+	current_save.player_x = current_player.global_position.x
+	current_save.player_y = current_player.global_position.y
+	current_save.player_info.first_name = PlayerStats.first_name
+	current_save.player_info.last_name = PlayerStats.last_name
+	current_save.player_info.background = PlayerStats.background
+	current_save.player_info.player_context = PlayerStats.player_context
+	current_save.player_info.current_money = PlayerStats.current_money
+	current_save.game_time_variables.game_time_seconds = PlayerManager.game_time_seconds
+	current_save.game_time_variables.game_time_hours = PlayerManager.game_time_hours
+	current_save.game_time_variables.game_time_minutes = PlayerManager.game_time_minutes
+
+func update_lvling_attributes() -> void:
+	current_save.player_lvling_info.lvl = PlayerStats.lvl
+	current_save.player_lvling_info.current_exp = PlayerStats.current_exp
+	current_save.player_lvling_info.current_exp_threshold = PlayerStats.current_exp_threshold
+	current_save.player_attributes.vigor = PlayerStats.vigor
+	current_save.player_attributes.arcane = PlayerStats.arcane
+	current_save.player_attributes.vitality = PlayerStats.vitality
+	current_save.player_attributes.alacrity = PlayerStats.alacrity
+	current_save.player_attributes.fortune = PlayerStats.fortune
+	current_save.player_attributes.max_hp = PlayerStats.max_hp
+	current_save.player_attributes.max_mp = PlayerStats.max_mp
+	current_save.player_attributes.max_stm = PlayerStats.max_stm
+	current_save.player_attributes.current_hp = PlayerStats.current_hp
+	current_save.player_attributes.current_mp = PlayerStats.current_mp
+	current_save.player_attributes.current_stm = PlayerStats.current_stm
+
+func update_topics() -> void:
+	current_save.player_gen_topics.current_save.player_gen_topics.weapons = PlayerStats.weapons
+	current_save.player_gen_topics.health = PlayerStats.health
+	current_save.player_gen_topics.monsters = PlayerStats.monsters
+	current_save.player_gen_topics.literature = PlayerStats.literature
+	current_save.player_gen_topics.nature = PlayerStats.nature
+	current_save.player_gen_topics.romance = PlayerStats.romance
+	current_save.player_gen_topics.subterfuge = PlayerStats.subterfuge
+	current_save.player_gen_topics.commerce = PlayerStats.commerce
+	current_save.player_gen_topics.games = PlayerStats.games
+	current_save.player_gen_topics.mysteries = PlayerStats.mysteries
+	current_save.player_unique_topics = PlayerStats.unique_topics
