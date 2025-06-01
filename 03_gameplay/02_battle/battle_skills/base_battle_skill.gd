d79f8215eec78576c9e853152609ba7df5abc03f
Meinos Kaen~ <jpde@tutanota.com>
2025-06-01 18:47:23 +0200

    We starting again. And to not lose ourselves, let's go in sequence like we're building a demo.

--- 03_gameplay/02_battle/battle_skills/base_battle_skill.gd
+++ 03_gameplay/02_battle/battle_skills/base_battle_skill.gd
@@ -5,5 +5,8 @@ class_name BattleSkill extends Node
 @export var battleskill_power : int = 1
 @export var battleskill_icon : Texture2D = null
 
-@export_enum("Physical Damage", "Ranged Damage", "Magic Damage", "Recovery Only", "Debuff Only", "Buff Only", "Passive") var battleskill_type : String = "Physical Damage"
-@export_enum("Fire", "Ice", "Lightning") var battleskill_element : String = "Fire"
+@export_enum("Melee Damage", "Ranged Damage", "Magic Damage", "Recovery Only", "Debuff Only", "Buff Only", "Passive") var battleskill_type : String = "Melee Damage"
+@export_enum("Fire", "Water", "Earth", "Wind", "Melee", "Ranged", "Lightning", "Ice", "Rot", "Growth", 
+"Gravity", "Levitation", "Explosion", "Healing", "Ruin", "Psychosis") var battleskill_element : String = "None"
+@export_enum("Single Enemy", "Single Ally", "All Enemies", "All Allies", "Everyone") var battleskill_target : String = "Single Enemy"
+@export var status_effect = null#: StatusEffect
--- 03_gameplay/02_battle/battlers/base_battler.gd
+++ 03_gameplay/02_battle/battlers/base_battler.gd
@@ -10,6 +10,8 @@ class_name Battler extends Node
 @export var battler_max_hp : int = 1
 @export var battler_mp : int = 1
 @export var battler_max_mp : int = 1
+@export var battler_ap : int = 1
+@export var battler_max_ap : int = 1
 
 @export_group("Attributes")
 @export var battler_vigor : int = 1
@@ -18,14 +20,26 @@ class_name Battler extends Node
 @export var battler_alacrity : int = 1
 @export var battler_fortune : int = 1
 
+@export_group("Resistances")
+@export_enum("Fire", "Water", "Earth", "Wind", "Melee", "Ranged", "Lightning", "Ice", "Rot", "Growth", 
+"Gravity", "Levitation", "Explosion", "Healing", "Ruin", "Psychosis") var battler_resist : String = "None"
+@export_enum("Fire", "Water", "Earth", "Wind", "Melee", "Ranged", "Lightning", "Ice", "Rot", "Growth", 
+"Gravity", "Levitation", "Explosion", "Healing", "Ruin", "Psychosis") var battler_weak : String = "None"
+@export_enum("Fire", "Water", "Earth", "Wind", "Melee", "Ranged", "Lightning", "Ice", "Rot", "Growth", 
+"Gravity", "Levitation", "Explosion", "Healing", "Ruin", "Psychosis") var battler_absorb : String = "None"
+@export_enum("Fire", "Water", "Earth", "Wind", "Melee", "Ranged", "Lightning", "Ice", "Rot", "Growth", 
+"Gravity", "Levitation", "Explosion", "Healing", "Ruin", "Psychosis") var battler_nullify : String = "None"
+@export_enum("Fire", "Water", "Earth", "Wind", "Melee", "Ranged", "Lightning", "Ice", "Rot", "Growth", 
+"Gravity", "Levitation", "Explosion", "Healing", "Ruin", "Psychosis") var battler_reflect : String = "None"
+
 @export_group("Battle Skills")
-@export var battler_attack : BattleSkill = null
 @export var battler_skill_1 : BattleSkill = null
 @export var battler_skill_2 : BattleSkill = null
 @export var battler_skill_3 : BattleSkill = null
 @export var battler_skill_4 : BattleSkill = null
 @export var battler_skill_5 : BattleSkill = null
-@export var battler_skill_6 : BattleSkill = null
-@export var battler_skill_7 : BattleSkill = null
-@export var battler_skill_8 : BattleSkill = null
-@export var battler_defend : BattleSkill = null
+
+@export_group("Passive Skills")
+@export var battler_passive_skill_1 : BattleSkill = null
+@export var battler_passive_skill_2 : BattleSkill = null
+@export var battler_passive_skill_3 : BattleSkill = null
